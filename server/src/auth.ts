cf3bb7e9bb99ffe1de35e54ef39097c2938555a7
Dhemes Mota <dhemes.mota@gmail.com>
2021-06-13 16:48:23 -0300

    permissions control

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -6,7 +6,7 @@ import { createRefreshToken } from './database';
 export function generateJwtAndRefreshToken(email: string, payload: object = {}) {
   const token = jwt.sign(payload, auth.secret, {
     subject: email,
-    expiresIn: 10, // 5 === 5 seconds
+    expiresIn: 60 * 15, // 15 minutes
   });
 
   const refreshToken = createRefreshToken(email)
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,9 +3,10 @@ import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
+import { Can } from '../components/Can'
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me')
@@ -14,7 +15,15 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
+      <Can permissions={['metrics.list']}>
+        <div>Metrics</div>
+      </Can>
+    </>
   )
 }
 
